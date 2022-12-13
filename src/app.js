4df6db76a6b2762b2fb4a4c0e1a39b34760f057f
Emanuel Ciotoi <emanuel12m@gmail.com>
2022-12-13 19:58:20 +0000

    Ejercicio terminado con los 2 extras validos

--- src/app.js
+++ src/app.js
@@ -7,5 +7,29 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  document.querySelector("p.alert").innerHTML = "Check <b>console</b>.";
+
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon", "us"];
+  let domains = [".com", ".net", ".us", ".io"];
+
+  pronoun.forEach(itPron => {
+    adj.forEach(itAdj => {
+      noun.forEach(itNoun => {
+        domains.forEach(itDom => {
+          let end = itDom.substring(1);
+
+          if (!itNoun.endsWith(end)) {
+            console.log(itPron + itAdj + itNoun + itDom);
+          } else {
+            let index = itNoun.lastIndexOf(end);
+            let sust = itNoun.substring(0, index);
+
+            console.log(itPron + itAdj + sust + itDom);
+          }
+        });
+      });
+    });
+  });
 };
